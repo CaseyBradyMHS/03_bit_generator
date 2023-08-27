866f5d598612d3b4d868df1ff9343a4590ed23c8
CaseyBradyMHS <140982529+CaseyBradyMHS@users.noreply.github.com>
2023-08-28 09:59:04 +1200

    Add files via upload

---
+++ integer_checker.py
@@ -0,0 +1,25 @@
+# checks input is a number more than a given value
+
+def num_check(question, low):
+    valid = False
+    while not valid:
+
+        error = "please enter a integer that is more than "
+        "(or equal to) {}".format(low)
+
+        try:
+            # ask for a number
+            response = int(input(question))
+
+            # check number is more than 0
+            if response >= low:
+                return response
+
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+            print()
